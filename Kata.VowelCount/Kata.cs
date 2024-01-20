fe4e64e2a6107d2dfa06edc86c60c128d01bbda6
Alejandro Junqui <ajunquit@gmail.com>
2024-01-19 22:15:28 -0500

    Update kata vowel count.

--- Kata.VowelCount/Kata.cs
+++ Kata.VowelCount/Kata.cs
@@ -6,12 +6,6 @@ public static class Kata
 {
     public static int GetVowelCount(string str)
     {
-        int vowelCount = 0;
-
-        string[] vowels = ["a", "e", "i", "o", "u"];
-
-        vowelCount = str.Where(x => vowels.Contains(x.ToString())).Count();
-
-        return vowelCount;
+        return str.Count(vowel => "aeiou".Contains(vowel));
     }
 }
