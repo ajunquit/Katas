6741e49a977f62155908654ac2fa6ae4721d6044
Alejandro Junqui <ajunquit@gmail.com>
2024-01-20 11:05:07 -0500

    Add Test case for Grouped by commas project.

--- Kata.Strings.Tests/StringsTests.cs
+++ Kata.Strings.Tests/StringsTests.cs
@@ -10,4 +10,19 @@ public class StringsTests
         int expectedValue = 5;
         Assert.That(expectedValue, Is.EqualTo(vowelsCount));
     }
+
+    [Test]
+    public void Test_GroupedByCommas()
+    {
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(1), Is.EqualTo("1"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(12), Is.EqualTo("12"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(123), Is.EqualTo("123"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(1234), Is.EqualTo("1,234"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(12345), Is.EqualTo("12,345"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(123456), Is.EqualTo("123,456"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(1234567), Is.EqualTo("1,234,567"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(12345678), Is.EqualTo("12,345,678"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(123456789), Is.EqualTo("123,456,789"));
+        Assert.That(GroupedByCommas.Kata.GroupByCommas(1234567890), Is.EqualTo("1,234,567,890"));
+    }
 }
