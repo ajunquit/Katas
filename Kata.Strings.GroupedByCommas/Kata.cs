fd184e49fe4c93471a5d3c4563f4826faad9f58a
Alejandro Junqui <ajunquit@gmail.com>
2024-01-20 11:35:31 -0500

    Add solution for grouped by commas Kata

---
+++ Kata.Strings.GroupedByCommas/Kata.cs
@@ -0,0 +1,36 @@
+﻿namespace Kata.Strings.GroupedByCommas;
+
+using System.Linq;
+
+public static class Kata
+{
+    public static string GroupByCommas(int n)
+    {
+        var values = n.ToString();
+        int lenght = values.Length;
+        int groupBy = 3;
+        int remainder = lenght % groupBy;
+        int divide = lenght / groupBy;
+        string chunks = string.Empty;
+
+        string chunk = GetChunk(values, 0, remainder);
+
+        chunks += !string.IsNullOrEmpty(chunk) && divide > 0 ? chunk + "," : chunk;
+
+        remainder++;
+
+        for (int i = remainder; i < lenght; i = i + groupBy)
+        {
+            chunks += GetChunk(values, remainder - 1, groupBy);
+            chunks += (remainder + groupBy - 1 != lenght) ? "," : string.Empty;
+            remainder = remainder + groupBy;
+        }
+
+        return chunks;
+    }
+
+    private static string GetChunk(string values, int from, int distance)
+    {
+        return values.Substring(from, distance);
+    }
+}
